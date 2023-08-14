58612ee8a18ece12af4ab10cf60e524511562c1a
zer0 <osom8979@gmail.com>
2023-08-14 16:53:55 +0900

    Update class: CvlContours

--- cvlayer/cv/contours.py
+++ cvlayer/cv/contours.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from enum import Enum, unique
-from typing import Iterable
+from typing import Iterable, NamedTuple
 
 import cv2
 from numpy import array, logical_and, ndarray, zeros
 from numpy.typing import NDArray
 
-from cvlayer.types import Rect
+from cvlayer.types import PointFloat, Rect, SizeFloat
 
 
 @unique
@@ -25,6 +25,12 @@ class FindContoursMethod(Enum):
     SIMPLE = cv2.CHAIN_APPROX_SIMPLE
     TC89_KCOS = cv2.CHAIN_APPROX_TC89_KCOS
     TC89_L1 = cv2.CHAIN_APPROX_TC89_L1
+
+
+class MinAreaRectResult(NamedTuple):
+    center: PointFloat
+    size: SizeFloat
+    rotation: float
 
 
 def find_largest_contour_index(contours: Iterable[NDArray], oriented=False) -> int:
@@ -57,3 +63,30 @@ def bitwise_intersection_contours(
     mask2 = cv2.drawContours(blank2, [contour2], 1, 1)  # noqa
 
     return logical_and(mask1, mask2)
+
+
+def contour_area(contour: NDArray, oriented=False) -> float:
+    return cv2.contourArea(contour, oriented)
+
+
+def convex_hull(contour: NDArray) -> NDArray:
+    return cv2.convexHull(contour)
+
+
+def arc_length(curve: NDArray, closed=False) -> float:
+    return cv2.arcLength(curve, closed=closed)
+
+
+def approx_poly_dp(curve: NDArray, epsilon: float, closed=False) -> NDArray:
+    return cv2.approxPolyDP(curve, epsilon=epsilon, closed=closed)
+
+
+def min_area_rect(points: NDArray) -> MinAreaRectResult:
+    center, size, rotation = cv2.minAreaRect(points)
+    cx, cy = center
+    w, h = size
+    return MinAreaRectResult((cx, cy), (w, h), rotation)
+
+
+def box_points(box: MinAreaRectResult) -> NDArray:
+    return cv2.boxPoints(box)
--- cvlayer/cvl/contours.py
+++ cvlayer/cvl/contours.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import cv2
 from numpy.typing import NDArray
 
-from cvlayer.cv.contours import convert_roi2contour, find_largest_contour_index
+from cvlayer.cv.contours import (
+    MinAreaRectResult,
+    approx_poly_dp,
+    arc_length,
+    box_points,
+    contour_area,
+    convert_roi2contour,
+    convex_hull,
+    find_largest_contour_index,
+    min_area_rect,
+)
 from cvlayer.types import Rect
 
 
 class CvlContours:
-    @staticmethod
-    def cvl_contour_area(contour: NDArray, oriented=False) -> float:
-        return cv2.contourArea(contour, oriented)
+    MinAreaRectResultType = MinAreaRectResult
 
     @staticmethod
     def cvl_find_largest_contour_index(contour: NDArray, oriented=False) -> int:
@@ -19,3 +26,27 @@ class CvlContours:
     @staticmethod
     def cvl_convert_roi2contour(roi: Rect) -> NDArray:
         return convert_roi2contour(roi)
+
+    @staticmethod
+    def cvl_contour_area(contour: NDArray, oriented=False) -> float:
+        return contour_area(contour, oriented)
+
+    @staticmethod
+    def cvl_convex_hull(contour: NDArray) -> NDArray:
+        return convex_hull(contour)
+
+    @staticmethod
+    def cvl_arc_length(curve: NDArray, closed=False) -> float:
+        return arc_length(curve, closed)
+
+    @staticmethod
+    def cvl_approx_poly_dp(curve: NDArray, epsilon: float, closed=False) -> NDArray:
+        return approx_poly_dp(curve, epsilon, closed)
+
+    @staticmethod
+    def cvl_min_area_rect(points: NDArray) -> MinAreaRectResult:
+        return min_area_rect(points)
+
+    @staticmethod
+    def cvl_box_points(box: MinAreaRectResult) -> NDArray:
+        return box_points(box)
