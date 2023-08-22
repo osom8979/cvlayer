12a4aed3166725e91d60b2f46fd398ac62a3dd72
zer0 <osom8979@gmail.com>
2023-08-23 00:57:43 +0900

    Create method: intersection_polygon

--- cvlayer/cv/contours_shapely.py
+++ cvlayer/cv/contours_shapely.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from typing import List
+from itertools import chain
+from typing import List, Optional
 
-from numpy import array, int32
 from numpy.typing import NDArray
-from shapely.geometry import LineString, MultiPolygon, Polygon
+from shapely.geometry import (
+    GeometryCollection,
+    LinearRing,
+    LineString,
+    MultiLineString,
+    MultiPoint,
+    MultiPolygon,
+    Point,
+    Polygon,
+)
 from shapely.geometry.base import BaseGeometry
-from shapely.ops import unary_union
-
-from cvlayer.types import Point
 
 
 def convert_contour2polygon(contour: NDArray) -> Polygon:
@@ -27,31 +33,58 @@ def convert_contour2linestring(contour: NDArray) -> LineString:
     return LineString(contour[:, 0, :].tolist())
 
 
-def convert_line2linestring(p1: Point, p2: Point) -> LineString:
-    return LineString([[p1[0], p1[1]], [p2[0], p2[1]]])
+def convert_line2linestring(x1, y1, x2, y2) -> LineString:
+    return LineString([[x1, y1], [x2, y2]])
 
 
-def intersection_polygons(polygon1: Polygon, polygon2: Polygon) -> Polygon:
-    intersection = polygon1.intersection(polygon2)
-    if intersection.geom_type == "Polygon":
-        assert isinstance(intersection, Polygon)
-        return intersection
+def filter_polygons(base: Optional[BaseGeometry]) -> List[Polygon]:
+    if base is None:
+        return list()
+    elif base.is_empty:
+        return list()
+    elif base.geom_type == "GeometryCollection":
+        assert isinstance(base, GeometryCollection)
+        return list(chain(*[filter_polygons(geom) for geom in base.geoms]))
+    elif base.geom_type == "MultiPolygon":
+        assert isinstance(base, MultiPolygon)
+        return list(chain(*[filter_polygons(geom) for geom in base.geoms]))
+    elif base.geom_type == "Polygon":
+        assert isinstance(base, Polygon)
+        return [base]
+    elif base.geom_type == "MultiLineString":
+        assert isinstance(base, MultiLineString)
+        return list()
+    elif base.geom_type == "LinearRing":
+        assert isinstance(base, LinearRing)
+        return list()
+    elif base.geom_type == "LineString":
+        assert isinstance(base, LineString)
+        return list()
+    elif base.geom_type == "MultiPoint":
+        assert isinstance(base, MultiPoint)
+        return list()
+    elif base.geom_type == "Point":
+        assert isinstance(base, Point)
+        return list()
     else:
-        union = unary_union(list(intersection))
-        assert isinstance(union, (Polygon, MultiPolygon))
+        raise TypeError(f"Unsupported geom type: {base.geom_type}")
 
 
-def geometry_to_contours(geom: BaseGeometry) -> List[NDArray]:
-    if geom.geom_type == "Polygon":
-        assert isinstance(geom, Polygon)
-        exterior_coords = array(geom.exterior.coords, dtype=int32)
-        return [exterior_coords]
-    elif geom.geom_type == "MultiPolygon":
-        assert isinstance(geom, MultiPolygon)
-        contours = []
-        for p in geom:
-            exterior_coords = array(p.exterior.coords, dtype=int32)
-            contours.append(exterior_coords)
-        return contours
-    else:
-        raise ValueError("Unsupported geometry type")
+def intersection_polygon(polygon1: Polygon, polygon2: Polygon) -> List[Polygon]:
+    return filter_polygons(polygon1.intersection(polygon2))
+
+
+# def geometry_to_contours(geom: BaseGeometry) -> List[NDArray]:
+#     if geom.geom_type == "Polygon":
+#         assert isinstance(geom, Polygon)
+#         exterior_coords = array(geom.exterior.coords, dtype=int32)
+#         return [exterior_coords]
+#     elif geom.geom_type == "MultiPolygon":
+#         assert isinstance(geom, MultiPolygon)
+#         contours = []
+#         for p in geom:
+#             exterior_coords = array(p.exterior.coords, dtype=int32)
+#             contours.append(exterior_coords)
+#         return contours
+#     else:
+#         raise ValueError("Unsupported geometry type")
--- tester/cv/test_contours_shapely.py
+++ tester/cv/test_contours_shapely.py
@@ -4,20 +4,59 @@ from unittest import TestCase, main
 
 from shapely.geometry import Polygon
 
-from cvlayer.cv.contours_shapely import intersection_polygons
+from cvlayer.cv.contours_shapely import intersection_polygon
 
 
 class ContoursShapelyTestCase(TestCase):
-    def test_intersection_polygons(self):
+    def test_intersection_polygon_single_polygon(self):
+        # ==========================
+        #           +y             |
+        #            |             |
+        #            |             |
+        #     (-1,1) |             |
+        #        *-------*         |
+        #        |   |   |         |
+        # -x ----|---*---|---*--- +x
+        #        |   |   |   |     |
+        #        *-------*   |     |
+        #            |       |     |
+        #            *-------*     |
+        #            |     (2,-2)  |
+        #           -y             |
+        # ==========================
         roi1 = (-1, 1), (1, 1), (1, -1), (-1, -1)
         roi2 = (0, 0), (2, 0), (2, -2), (0, -2)
         polygon1 = Polygon(roi1)
         polygon2 = Polygon(roi2)
-        result = intersection_polygons(polygon1, polygon2)
+        result = intersection_polygon(polygon1, polygon2)
+        self.assertEqual(1, len(result))
 
         expected_bbox = [0, -1, 1, 0]
-        result_bbox = list(round(i) for i in result.bounds)
+        result_bbox = list(round(i) for i in result[0].bounds)
         self.assertEqual(expected_bbox, result_bbox)
+
+    def test_intersection_polygon_single_line(self):
+        # ==========================
+        #           +y             |
+        #            |     (2,2)   |
+        #            *-------*     |
+        #            |       |     |
+        #            |       |     |
+        #            |       |     |
+        # -x --------*-------*--- +x
+        #            |       |     |
+        #            |       |     |
+        #            |       |     |
+        #            *-------*     |
+        #            |     (2,-2)  |
+        #           -y             |
+        # ==========================
+        roi1 = (0, 2), (2, 2), (2, 0), (0, 0)
+        roi2 = (0, 0), (2, 0), (2, -2), (0, -2)
+        polygon1 = Polygon(roi1)
+        polygon2 = Polygon(roi2)
+        result = intersection_polygon(polygon1, polygon2)
+        self.assertEqual(0, len(result))
 
 
 if __name__ == "__main__":
