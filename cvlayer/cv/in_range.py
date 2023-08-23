8075c2bfeda22dc6174d016a9ef787541f215064
zer0 <osom8979@gmail.com>
2023-08-23 20:04:11 +0900

    Create function: in_range2

--- cvlayer/cv/in_range.py
+++ cvlayer/cv/in_range.py
@@ -4,5 +4,17 @@ import cv2
 from numpy.typing import NDArray
 
 
-def in_range(src: NDArray, lower_bound, upper_bound) -> NDArray:
+def in_range(src: NDArray, lower_bound: NDArray, upper_bound: NDArray) -> NDArray:
     return cv2.inRange(src, lower_bound, upper_bound)
+
+
+def in_range2(
+    src: NDArray,
+    lower_bound1: NDArray,
+    upper_bound1: NDArray,
+    lower_bound2: NDArray,
+    upper_bound2: NDArray,
+) -> NDArray:
+    mask1 = cv2.inRange(src, lower_bound1, upper_bound1)
+    mask2 = cv2.inRange(src, lower_bound2, upper_bound2)
+    return cv2.bitwise_or(mask1, mask2)
--- cvlayer/cvl/in_range.py
+++ cvlayer/cvl/in_range.py
@@ -2,10 +2,24 @@
 
 from numpy.typing import NDArray
 
-from cvlayer.cv.in_range import in_range
+from cvlayer.cv.in_range import in_range, in_range2
 
 
 class CvlInRange:
     @staticmethod
-    def cvl_in_range(src: NDArray, lower_bound, upper_bound) -> NDArray:
+    def cvl_in_range(
+        src: NDArray,
+        lower_bound: NDArray,
+        upper_bound: NDArray,
+    ) -> NDArray:
         return in_range(src, lower_bound, upper_bound)
+
+    @staticmethod
+    def cvl_in_range2(
+        src: NDArray,
+        lower_bound1: NDArray,
+        upper_bound1: NDArray,
+        lower_bound2: NDArray,
+        upper_bound2: NDArray,
+    ) -> NDArray:
+        return in_range2(src, lower_bound1, upper_bound1, lower_bound2, upper_bound2)
