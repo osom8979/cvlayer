e988a4b2a868ab77aa16e18a82fc9823d2e9ba99
zer0 <osom8979@gmail.com>
2023-10-12 10:45:53 +0900

    Update version: 0.12.0

--- cvlayer/__init__.py
+++ cvlayer/__init__.py
@@ -4,7 +4,7 @@ from cvlayer.cvlayer import CvLayer
 from cvlayer.cvmanager import CvManager
 from cvlayer.cvwindow import CvWindow
 
-__version__ = "0.11.0"
+__version__ = "0.12.0"
 __all__ = [
     "__version__",
     "CvLayer",
