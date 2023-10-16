f1af4e2a5183d09361f1a168108cc54d0ab33429
zer0 <osom8979@gmail.com>
2023-10-17 00:18:54 +0900

    Create CvmCanny

--- cvlayer/cvmixin.py
+++ cvlayer/cvmixin.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from cvlayer.layer.manager.mixins.blur import CvmBlur
+from cvlayer.layer.manager.mixins.canny import CvmCanny
 from cvlayer.layer.manager.mixins.cvt_color import CvmCvtColor
 from cvlayer.layer.manager.mixins.morphology import CvmMorphology
 
 
 class CvMixin(
     CvmBlur,
+    CvmCanny,
     CvmCvtColor,
     CvmMorphology,
 ):
---
+++ cvlayer/layer/manager/mixins/canny.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from cvlayer.cv.canny import CANNY_THRESHOLD_MAX, CANNY_THRESHOLD_MIN, canny
+from cvlayer.layer.manager.mixins._base import _LayerManagerMixinBase
+
+
+class CvmCanny(_LayerManagerMixinBase):
+    def cvm_canny(self, th_min=CANNY_THRESHOLD_MIN, th_max=CANNY_THRESHOLD_MAX):
+        with self.layer("cvm_canny") as layer:
+            th_min = layer.param("th_min").build_unsigned(th_min).value
+            th_max = layer.param("th_max").build_unsigned(th_max).value
+            result = canny(layer.prev_frame, th_min, th_max)
+            layer.frame = result
+        return result
