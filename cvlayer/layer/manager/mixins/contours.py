0336e6e72f6fde9f4d7da02d0999bc8ac00147fe
zer0 <osom8979@gmail.com>
2023-10-25 14:36:44 +0900

    More cvm_find_contours_* methods

--- cvlayer/layer/manager/mixins/contours.py
+++ cvlayer/layer/manager/mixins/contours.py
@@ -20,15 +20,26 @@ from cvlayer.layer.manager.mixins._base import LayerManagerMixinBase
 from cvlayer.palette import xkcd_palette
 from cvlayer.typing import Color
 
+_CCOMP = FindContoursMode.CCOMP
+_EXTERNAL = FindContoursMode.EXTERNAL
+_LIST = FindContoursMode.LIST
+_TREE = FindContoursMode.TREE
+_FLOODFILL = FindContoursMode.FLOODFILL
+
+_NONE = FindContoursMethod.NONE
+_SIMPLE = FindContoursMethod.SIMPLE
+_TC89_KCOS = FindContoursMethod.TC89_KCOS
+_TC89_L1 = FindContoursMethod.TC89_L1
+
 
 class CvmContours(LayerManagerMixinBase):
-    def cvm_find_contours(
+    def _cvm_find_contours(
         self,
         name: str,
-        mode=FindContoursMode.TREE,
-        method=FindContoursMethod.SIMPLE,
-        area_min=1_000.0,
-        area_max=500_000.0,
+        mode: FindContoursMode,
+        method: FindContoursMethod,
+        area_min: float,
+        area_max: float,
         canvas: Optional[NDArray] = None,
         palette: Optional[Dict[str, Color]] = None,
         frame: Optional[NDArray] = None,
@@ -73,3 +84,263 @@ class CvmContours(LayerManagerMixinBase):
             layer.frame = canvas
             layer.data = filtered_contours
         return canvas, filtered_contours
+
+    def cvm_find_contours_ccomp_none(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _CCOMP, _NONE, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_ccomp_simple(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _CCOMP, _SIMPLE, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_ccomp_tc89_kcos(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _CCOMP, _TC89_KCOS, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_ccomp_tc89_l1(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _CCOMP, _TC89_L1, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_external_none(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _EXTERNAL, _NONE, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_external_simple(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _EXTERNAL, _SIMPLE, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_external_tc89_kcos(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _EXTERNAL, _TC89_KCOS, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_external_tc89_l1(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _EXTERNAL, _TC89_L1, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_list_none(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _LIST, _NONE, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_list_simple(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _LIST, _SIMPLE, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_list_tc89_kcos(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _LIST, _TC89_KCOS, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_list_tc89_l1(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _LIST, _TC89_L1, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_tree_none(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _TREE, _NONE, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_tree_simple(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _TREE, _SIMPLE, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_tree_tc89_kcos(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _TREE, _TC89_KCOS, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_tree_tc89_l1(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _TREE, _TC89_L1, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_floodfill_none(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _FLOODFILL, _NONE, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_floodfill_simple(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _FLOODFILL, _SIMPLE, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_floodfill_tc89_kcos(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _FLOODFILL, _TC89_KCOS, area_min, area_max, canvas, palette, frame
+        )
+
+    def cvm_find_contours_floodfill_tc89_l1(
+        self,
+        name: str,
+        area_min=1_000.0,
+        area_max=500_000.0,
+        canvas: Optional[NDArray] = None,
+        palette: Optional[Dict[str, Color]] = None,
+        frame: Optional[NDArray] = None,
+    ):
+        return self._cvm_find_contours(
+            name, _FLOODFILL, _TC89_L1, area_min, area_max, canvas, palette, frame
+        )
