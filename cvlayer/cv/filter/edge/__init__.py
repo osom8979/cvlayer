bff91b09bd11e585e75af70f0e3a6f8d42f4610d
zer0 <osom8979@gmail.com>
2023-11-02 16:09:11 +0900

    Create CvlFilterEdgeScharr

--- cvlayer/cv/filter/edge/__init__.py
+++ cvlayer/cv/filter/edge/__init__.py
@@ -2,12 +2,14 @@
 
 from cvlayer.cv.filter.edge.canny import CvlFilterEdgeCanny
 from cvlayer.cv.filter.edge.laplacian import CvlFilterEdgeLaplacian
+from cvlayer.cv.filter.edge.scharr import CvlFilterEdgeScharr
 from cvlayer.cv.filter.edge.sobel import CvlFilterEdgeSobel
 
 
 class CvlFilterEdge(
     CvlFilterEdgeCanny,
     CvlFilterEdgeLaplacian,
+    CvlFilterEdgeScharr,
     CvlFilterEdgeSobel,
 ):
     pass
--- cvlayer/cv/filter/edge/scharr.py
+++ cvlayer/cv/filter/edge/scharr.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from typing import Final, Tuple
+from typing import Final
 
 import cv2
 from numpy.typing import NDArray
@@ -13,66 +13,53 @@ from cvlayer.cv.types.ddepth import (
 )
 
 DEFAULT_DX: Final[int] = 1
+"""order of the derivative x"""
+
 DEFAULT_DY: Final[int] = 1
-DEFAULT_KERNEL_SIZE: Final[int] = 3
+"""order of the derivative y"""
+
 DEFAULT_SCALE: Final[float] = 1.0
+"""
+optional scale factor for the computed derivative values;
+by default, no scaling is applied
+(see getDerivKernels for details)
+"""
+
 DEFAULT_DELTA: Final[float] = 0.0
-AVAILABLE_KERNEL_SIZE: Tuple[int, int, int, int] = 1, 3, 5, 7
+"""
+optional delta value that is added to the results prior to storing them in dst.
+"""
 
 
-def sobel(
-    frame: NDArray,
+def scharr(
+    src: NDArray,
     ddepth=DEFAULT_DESIRED_DEPTH,
     dx=DEFAULT_DX,
     dy=DEFAULT_DY,
-    kernel_size=DEFAULT_KERNEL_SIZE,
     scale=DEFAULT_SCALE,
     delta=DEFAULT_DELTA,
     border=DEFAULT_BORDER_TYPE,
 ) -> NDArray:
-    assert dx or dy
-    assert kernel_size % 2 == 1
-    assert kernel_size >= 1
-    assert kernel_size in AVAILABLE_KERNEL_SIZE
-
     _ddepth = normalize_desired_depth(ddepth)
-    validate_depth_combinations(frame, _ddepth)
+    validate_depth_combinations(src, _ddepth)
+
     _border = normalize_border_type(border)
+
     if _border == cv2.BORDER_WRAP:
         raise ValueError("Unsupported border type: BORDER_WRAP")
 
-    return cv2.Sobel(
-        frame,
-        _ddepth,
-        dx,
-        dy,
-        None,
-        kernel_size,
-        scale,
-        delta,
-        _border,
-    )
+    return cv2.Scharr(src, _ddepth, dx, dy, None, scale, delta, _border)
 
 
-class CvlFilterEdgeSobel:
+class CvlFilterEdgeScharr:
     @staticmethod
-    def cvl_sobel(
-        frame: NDArray,
+    def cvl_scharr(
+        src: NDArray,
         ddepth=DEFAULT_DESIRED_DEPTH,
         dx=DEFAULT_DX,
         dy=DEFAULT_DY,
-        kernel_size=DEFAULT_KERNEL_SIZE,
         scale=DEFAULT_SCALE,
         delta=DEFAULT_DELTA,
         border=DEFAULT_BORDER_TYPE,
     ):
-        return sobel(
-            frame,
-            ddepth,
-            dx,
-            dy,
-            kernel_size,
-            scale,
-            delta,
-            border,
-        )
+        return scharr(src, ddepth, dx, dy, scale, delta, border)
