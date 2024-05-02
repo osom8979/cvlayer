e8f8f8a1728eefa195b7f49242b1ef5866c7560d
zer0 <osom8979@gmail.com>
2024-05-02 10:31:51 +0900

    Bump version

--- cvlayer/__init__.py
+++ cvlayer/__init__.py
@@ -5,7 +5,7 @@ from cvlayer.cvwindow import CvWindow
 from cvlayer.layer.manager.cvmanager import CvManager
 from cvlayer.layer.manager.mixins import CvMixin
 
-__version__ = "0.30.0"
+__version__ = "0.30.1"
 __all__ = [
     "__version__",
     "CvLayer",
